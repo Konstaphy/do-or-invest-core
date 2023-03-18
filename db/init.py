9984c53eb20b5d07cf440adb122f0863e90fc507
Konstantine <Konstaphy@gmail.com>
2023-03-19 00:36:58 +0500

    Добавил ручки для добавления и чтения

---
+++ db/init.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+def init_database(cursor):
+    # query = """
+    #     DROP TABLE "Events"
+    #     """
+    query = """
+        CREATE TABLE IF NOT EXISTS "Events" (
+        "id"	TEXT NOT NULL UNIQUE,
+        "title"	TEXT NOT NULL,
+        "startDate"	TEXT NOT NULL,
+        "endDate"	TEXT NOT NULL,
+        "userId"	TEXT NOT NULL,
+        "done"	BOOLEAN NOT NULL,
+        PRIMARY KEY("id")
+    )
+    """
+    cursor.execute(query)
---
+++ src/main.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from db.init import init_database
+import uuid
+
+import sqlite3
+app = Flask(__name__)
+cors = CORS(app)
+
+with sqlite3.connect("db/database.db", check_same_thread=False) as db:
+    init_database(db.cursor())
+
+@app.route("/")
+def hello_world():
+    with sqlite3.connect("db/database.db", check_same_thread=False) as db:
+        cursor = db.cursor()
+        query = """select * from Events"""
+        cursor.execute(query)
+        records = cursor.fetchall()
+        cursor.close()
+        return jsonify({'events': records})
+
+@app.route("/event/new", methods=['POST'])
+def post_event():
+    input_json = request.get_json(force=True)
+    print(input_json["hello"])
+    with sqlite3.connect("db/database.db", check_same_thread=False) as db:
+        cursor = db.cursor()
+        query = """
+            insert into Events (id, title, startDate, endDate, userId, done) values (?, "123", "10-10-2022", "20-10-2022", ?, FALSE)
+        """
+        cursor.execute(query, (str(uuid.uuid4()), str(uuid.uuid4())))
+        cursor.close()
+
+    return "ready"
+
+app.run(debug=True)
